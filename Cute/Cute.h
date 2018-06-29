6209b3af3df3d647e871eaa8689d51c3d9e0afeb
ryanbaldwin <ryanbaldwin@gmail.com>
2018-06-29 09:28:38 -0400

    Added support for macOS & tvOS

--- Cute/Cute.h
+++ Cute/Cute.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018 bunnyhug.me All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Cute.
 FOUNDATION_EXPORT double CuteVersionNumber;
